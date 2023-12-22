4f1b38aa4b27110bab728f0a3a85bf35c2b8e54f
Jooyoung Jeong <jooyoungworknd@gmail.com>
2023-12-22 12:28:16 +0900

    requirements.txt added

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from google_translate import get_credential, google_texts
 import time
+import os
 
 FILEPATH = 'test.txt'
+# 언어코드는 해당 링크 참조
+# https://cloud.google.com/translate/docs/languages?hl=ko 
 SOURCE = 'ko'
 TARGET = 'en'
 UNIT = 1024
+DELAY = 3
 
 if __name__ == "__main__":
-    
     
     get_credential()
     final_output = []
@@ -19,7 +22,10 @@ if __name__ == "__main__":
     for i, chunk in enumerate(chunks):
         final_output.extend(google_texts(chunk, SOURCE, TARGET))
         print(f"chunk {i+1} done.")
-        time.sleep(3)
-        
-    with open(f'{FILEPATH}_{SOURCE}-{TARGET}', 'w', encoding='utf-8') as f:
+        time.sleep(DELAY)
+    
+    abspath = os.path.abspath(FILEPATH)
+    dir = os.path.dirname(abspath)
+    filename = ".".join(os.path.basename(abspath).split('.')[:-1])
+    with open(f'{dir}/{filename}_{SOURCE}-{TARGET}', 'w', encoding='utf-8') as f:
         f.write('\n'.join(final_output))
